42c637f59c959976a83ab585c5e0c75bddba95b3
Mayonk Kumar Behera <mayonk.kumar9@gmail.com>
2021-05-24 01:38:50 +0530

    final commit after learning from YouTube

---
+++ src/components/google-drive/Folder.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
+
+export default function Folder({ folder }) {
+  return (
+    <Button
+      to={{ pathname: `/folder/${folder.id}`, state: { folder: folder } }}
+      as={Link}
+      variant="outline-dark"
+      className="text-truncate w-100"
+    >
+      <FontAwesomeIcon icon={faFolder} className="mr-2" />
+      {folder.name}
+    </Button>
+  );
+}
